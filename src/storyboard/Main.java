0939a4ea0758e2219484fc81e81f1588d2afdbc5
Algalon2 <Algalon2@130.215.172.88>
2016-11-28 14:03:30 -0500

    Added Window opener

--- src/storyboard/Main.java
+++ src/storyboard/Main.java
@@ -1,5 +1,24 @@
 package storyboard;
 
-public class Main {
+import java.awt.EventQueue;
 
+import javax.swing.JFrame;
+
+public class Main extends JFrame{
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main frame = new Main();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	public Main(){
+		
+	}
 }
