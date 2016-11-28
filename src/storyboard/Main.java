4e8a6cee3dca5df11dd716a319a473bdb93012c7
Griffin Bishop <grbishop@wpi.edu>
2016-11-28 14:42:25 -0500

    The window is now the right size

--- src/storyboard/Main.java
+++ src/storyboard/Main.java
@@ -1,7 +1,10 @@
 package storyboard;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 public class Main extends JFrame{
 	public static void main(String[] args) {
@@ -18,6 +21,13 @@ public class Main extends JFrame{
 	}
 	
 	public Main(){
+		setTitle("LetterCraze Game");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
 		
+		JPanel contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
 	}
 }
