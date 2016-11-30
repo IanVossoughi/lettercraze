30e35cd18691ec615f2744dde577ae7988d7a62e
Griffin Bishop <grbishop@wpi.edu>
2016-11-30 14:52:18 -0500

    Added empty level save/load controller

---
+++ src/controllers/LevelSaveLoadController.java
@@ -0,0 +1,23 @@
+package controllers;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import builderBoundary.BuildField;
+
+public class LevelSaveLoadController implements ActionListener {
+
+	private BuildField builder;
+	
+	public LevelSaveLoadController(BuildField builder){
+		this.builder = builder;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent ae) {
+		
+		
+	}
+	
+	
+}
