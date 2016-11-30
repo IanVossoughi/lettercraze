69a49626f3a1ad425756700eb7d0d1b5930b2775
Batu Sipka <batusipka@gmail.com>
2016-11-30 13:36:43 -0500

    Controller for Exit button

---
+++ src/controllers/ExitButtonController.java
@@ -0,0 +1,22 @@
+package controllers;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import storyboard.MenuField;
+
+public class ExitButtonController implements ActionListener {
+
+	private MenuField menuField;
+
+	public ExitButtonController(MenuField menuField) {
+		this.menuField = menuField;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		menuField.setVisible(false);
+		menuField.dispose();
+	}
+
+}
