daf2880ebf78ae32920b5f8b0da773016da79b01
Gabsen <grodell@wpi.edu>
2016-11-30 14:58:13 -0500

    Fixed commit.

--- src/playerBoundary/MenuField.java
+++ src/playerBoundary/MenuField.java
@@ -3,7 +3,7 @@ package playerBoundary;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
-import javax.swing.JFrame;
+import javax.swing.JFrame; 
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
