30ac3532a765cd411f03ce700ea933a7001de3eb
Batu Sipka <batusipka@gmail.com>
2016-12-02 11:10:49 -0500

    Setup for Testing

---
+++ test/playerBoundary/TestOne.java
@@ -0,0 +1,14 @@
+package playerBoundary;
+
+import junit.framework.TestCase;
+
+public class TestOne extends TestCase {
+	
+	@Override
+	protected void setUp() {
+		
+	}
+	@Override
+	protected void tearDown() {
+	}
+}
