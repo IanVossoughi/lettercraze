50f57c94abe16655ba7c857994cbdae58af077c6
Griffin Bishop <grbishop@wpi.edu>
2016-12-06 13:27:03 -0500

    Fixed StarThresholdWindow so it wouldn't exit the whole program on clicking the red x.

--- src/builderBoundary/StarThresholdWindow.java
+++ src/builderBoundary/StarThresholdWindow.java
@@ -42,7 +42,7 @@ public class StarThresholdWindow extends JFrame {
 	 * Create the frame.
 	 */
 	public StarThresholdWindow() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 500, 400);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
