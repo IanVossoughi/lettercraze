50cb1b5cd1604a8397171b202c0eb6c6d69141e2
Ian Vossoughi <ivossoughi@wpi.edu>
2016-12-07 13:38:38 -0500

    PlayField loads tiles from the Model entity

--- src/controllers/PlayButtonController.java
+++ src/controllers/PlayButtonController.java
@@ -3,22 +3,25 @@ package controllers;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import entities.Model;
 import playerBoundary.MenuField;
 import playerBoundary.PlayField;
 
 public class PlayButtonController implements ActionListener{
 
 	private MenuField menuField;
+	private Model m;
 	//private int unlocked;
 
-	public PlayButtonController(MenuField menuField) {
+	public PlayButtonController(MenuField menuField, Model m) {
 		this.menuField = menuField;
+		this.m = m;
 		//this.unlocked = unlocked;
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		new PlayField().setVisible(true);
+		new PlayField(m).setVisible(true);
 		menuField.setVisible(false);
 		menuField.dispose();		
 	}
