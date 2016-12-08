6fb07e66c7649e8a57563469592c480e9f9504e1
Ian Vossoughi <ivossoughi@wpi.edu>
2016-12-08 13:35:27 -0500

    WordList will show submitted words (but resizes itself stupidly)

--- src/controllers/SubmitWordMove.java
+++ src/controllers/SubmitWordMove.java
@@ -4,18 +4,24 @@ import javax.swing.DefaultListModel;
 
 import entities.Model;
 import entities.Word;
+import playerBoundary.PlayField;
 
 public class SubmitWordMove {
 	private Word selectedWord;
 	private DefaultListModel<String> wordList;
+	private Model model;
+	private PlayField play;
 	
-	public SubmitWordMove(Model model) {
+	public SubmitWordMove(Model model, PlayField play) {
 		this.selectedWord = model.getSelectedWord();
 		this.wordList = model.getWordListModel();
+		this.model = model;
+		this.play = play;
 	}
 	public boolean doMove(){
 		if(isValid()){
 			wordList.addElement(selectedWord.getWordString());
+			play.refreshBoard();
 			return true;
 		}
 		return false;
