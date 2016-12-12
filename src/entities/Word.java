4d3c951ca514b09055255476c43358446026d6fb
andrewvanner <andrewvanner@gmail.com>
2016-12-12 14:34:41 -0500

    Word score works correctly. Function is sum of letters times length minus 2 (So a 3 letter word is times one)

--- src/entities/Word.java
+++ src/entities/Word.java
@@ -9,10 +9,49 @@ public class Word {
 		this.chars = chars;
 		this.score = score;
 	}
-	//Andrew. Score is only based on length right now, will update
+	//Andrew. Score is good?
 	public void addScore() {
-		int wordScore = chars.length();
+		int wordScore = 0;
+		int letterScore = 0;
+		for (int i = 0; i < chars.length(); i++) {
+			char letter = chars.charAt(i);
+			System.out.println(letter);
+			if ((letter == 'E') || (letter == 'T')){
+				letterScore = 1;
+				System.out.println(letterScore);
+			}
+			else if ((letter == 'A') || (letter == 'O') || (letter == 'I') || (letter == 'N')
+					|| (letter == 'S') || (letter == 'H') || (letter == 'R')){
+				letterScore = 2;
+				System.out.println(letterScore);
+			}
+			else if ((letter == 'D') || (letter == 'L') || (letter == 'C') || (letter == 'U')
+					|| (letter == 'M') || (letter == 'W')){
+				letterScore = 3;
+				System.out.println(letterScore);
+			}
+			else if ((letter == 'F') || (letter == 'G') || (letter == 'Y') || (letter == 'P')
+					|| (letter == 'B')){
+				letterScore = 4;
+				System.out.println(letterScore);
+			}
+			else if ((letter == 'V') || (letter == 'K')){
+				letterScore = 5;
+				System.out.println(letterScore);
+			}
+			else if ((letter == 'J') || (letter == 'X')){
+				letterScore = 7;
+				System.out.println(letterScore);
+			}
+			else if ((letter == 'Q') || (letter == 'Z')){
+				letterScore = 9;
+				System.out.println(letterScore);
+			}
+			wordScore = wordScore + letterScore; //Adds score up for every letter for the word
+			System.out.println(wordScore);
+		}
 		System.out.println(chars.length());
+		wordScore = wordScore * (chars.length() - 2); //Score is letters times (length - 2)
 		score = score + wordScore;
 		System.out.println(score);
 	}
