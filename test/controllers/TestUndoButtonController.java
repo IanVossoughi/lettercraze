55f451de42e61533ba9907fc40cc43e41501b4d5
Gabby O'Dell <grodell@wpi.edu>
2016-12-14 12:34:40 -0500

    TestUndoButtonController

--- test/controllers/TestUndoButtonController.java
+++ test/controllers/TestUndoButtonController.java
@@ -7,22 +7,25 @@ import playerBoundary.PlayField;
 
 public class TestUndoButtonController extends TestCase {
 	PlayField playField;
-	Model model;
+	Model m;
 	UndoArray undoArray;
 
 	@Override
 	protected void setUp() {
-		model = new Model();
-		playField = new PlayField(model);
+		m = new Model();
+		playField = new PlayField(m);
 		playField.setVisible(true);
 	}
 
 	public void testUndoButton() {
-		UndoButtonController ubc = new UndoButtonController(model, playField, undoArray);
+		UndoButtonController ubc = new UndoButtonController(m, playField, undoArray);
 		/*
 		ubc.actionPerformed(null);
-		int size = model.getWordListModel().getSize();
+		int size = m.getWordListModel().getSize();
 		assertEquals(0, size);
 		*/	
+		assertEquals("", undoArray.getLatestModel());
+		assertEquals(0, undoArray.getIndex());
+		assertTrue(undoArray.removeUndoModel());
 	}
 }
