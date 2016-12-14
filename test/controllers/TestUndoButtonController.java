1bad13bd4d91b4569660db2fcc90804a8e321e09
Gabby O'Dell <grodell@wpi.edu>
2016-12-14 16:41:53 -0500

    Test cases. 77.2% coverage.

--- test/controllers/TestUndoButtonController.java
+++ test/controllers/TestUndoButtonController.java
@@ -9,24 +9,26 @@ public class TestUndoButtonController extends TestCase {
 	PlayField playField;
 	Model m;
 	UndoArray undoArray;
+	int index;
 
 	@Override
 	protected void setUp() {
 		m = new Model();
+		m.setType(1);
+		index = m.getSelectedIndex();
 		playField = new PlayField(m);
 		playField.setVisible(true);
 	}
 
 	public void testUndoButton() {
 		UndoButtonController ubc = new UndoButtonController(m, playField, undoArray);
-		/*
 		ubc.actionPerformed(null);
-		int size = m.getWordListModel().getSize();
-		assertEquals(0, size);
-			
-		assertEquals("", undoArray.getLatestModel());
-		assertEquals(0, undoArray.getIndex());
-		assertTrue(undoArray.removeUndoModel());
-		*/
+		UndoArray ua = undoArray.getInstance();
+		
+		assertEquals(-1, ua.getIndex());
+		assertTrue(ua.addUndoModel(m));
+		assertEquals(0, ua.getIndex());
+		assertTrue(ua.removeUndoModel());
+		assertEquals(-1, ua.getIndex());
 	}
 }
