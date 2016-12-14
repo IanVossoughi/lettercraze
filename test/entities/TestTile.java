cb2e6f7234624b3640df3099efc1ec78a1099509
Gabby O'Dell <grodell@wpi.edu>
2016-12-14 10:55:31 -0500

    Test cases. 75.4% coverage.

--- test/entities/TestTile.java
+++ test/entities/TestTile.java
@@ -7,6 +7,9 @@ public class TestTile extends TestCase {
 	int y;
 	Model m;
 	Tile tile;
+	Tile tile2;
+	Board b;
+	Coordinate coords;
 	
 	@Override
 	protected void setUp() {
@@ -14,16 +17,40 @@ public class TestTile extends TestCase {
 		y = 1;
 		
 		m = new Model();
+		b = new Board();
 		tile = m.getBoard().getTile(x, y);
 		tile.setSelection(true);
 	}
 	
-	public void testToggleSelection() {
+	public void testTile() {
+		
+		// Testing toggleSelected()
 		assertTrue(tile.isSelected());
 		tile.toggleSelected();
 		assertFalse(tile.isSelected());
 		
 		tile.toggleSelected();
 		assertTrue(tile.isSelected());
+		
+		// Testing tile location on board
+		b.setTile(2, 2, tile2);
+		assertEquals(tile2, b.getTile(2, 2));
+		
+		// Testing tile coordinates
+		coords = new Coordinate(3, 3);
+		b.setSelectedTileCoords(coords);
+		assertEquals(coords, b.getSelectedTileCoords());
+		
+		// Testing enabling and replacing
+		tile.setEnabled(true);
+		tile.setReplacement(true);
+		assertTrue(tile.getReplacement());
+		assertTrue(tile.isEnabled());
+		
+		tile.setEnabled(false);
+		tile.setReplacement(false);
+		assertFalse(tile.getReplacement());
+		assertFalse(tile.isEnabled());	
+		
 	}
 }
