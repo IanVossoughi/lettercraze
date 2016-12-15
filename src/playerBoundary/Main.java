9baa8610f768b040b2305a021b84efebb639f732
Gabby O'Dell <grodell@wpi.edu>
2016-12-15 17:49:05 -0500

    Main splashscreen

--- src/playerBoundary/Main.java
+++ src/playerBoundary/Main.java
@@ -35,7 +35,7 @@ public class Main extends JFrame{
 		setTitle("LetterCraze Game");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		setBounds(100, 100, 700, 600);
+		setBounds(100, 100, 600, 500);
 	
 		JPanel contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
