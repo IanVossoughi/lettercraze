b371e7f1902f126db45cc22fd8cbf658a0044d67
Gabby O'Dell <grodell@wpi.edu>
2016-12-14 22:09:08 -0500

    Added test case. 81.0% coverage.

--- test/controllers/TestPlayerClickTileController.java
+++ test/controllers/TestPlayerClickTileController.java
@@ -6,27 +6,38 @@ import junit.framework.TestCase;
 import playerBoundary.PlayField;
 
 public class TestPlayerClickTileController extends TestCase {
-	int x;
-	int y;
+	int x, x1;
+	int y, y1;
 	PlayField pf;
 	Model m;
-	Tile tile;
+	Tile tile, tile2;
 
 	@Override
 	protected void setUp() {
 		m = new Model();
 		pf = new PlayField(m);
+		
 		x = 1;
 		y = 1;
 		tile = m.getBoard().getTile(x, y);
 		tile.setLetter("Q");
+		
+		x1 = 2;
+		y1 = 2;
+		tile = m.getBoard().getTile(x1, y1);
+		tile.setLetter("U");
 	}
 
 	public void testPlayerClickTile() {
+		
+		// Testing Q becomes QU
 		PlayerClickTileController pct = new PlayerClickTileController(m, pf, x, y);
 		pct.actionPerformed(null);
-		
 		assertEquals("QU", m.getBoard().getTile(x, y).getLetter());
 		
+		// Testing other letter
+		PlayerClickTileController pct1 = new PlayerClickTileController(m, pf, x1, y1);
+		pct1.actionPerformed(null);
+		assertEquals("U", m.getBoard().getTile(x1, y1).getLetter());
 	}
 }
