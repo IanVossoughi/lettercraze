348e78ce1974b7301df85f1069d702542b7632a8
Ian Vossoughi <ivossoughi@wpi.edu>
2016-12-15 18:50:00 -0500

    Four more controllers have documentation

--- src/controllers/ContinueButtonController.java
+++ src/controllers/ContinueButtonController.java
@@ -8,13 +8,29 @@ import playerBoundary.Main;
 import playerBoundary.MenuField;
 import playerBoundary.ProgressIO;
 
+/**
+ *  Controls all actions related to the Game Continue Button, 
+ *  specifically moving from the splash screen into the menu screen.
+ * <p>
+ * Creation date: (12/15/16 6:29:12 PM)
+ * @author Ian Vossoughi
+ */
 public class ContinueButtonController implements ActionListener{
 
+	/** The LetterCraze main that contains the splash screen and initiates the PlayField boundary. */
 	private Main main;
 
+	/**
+	 * ContinueBuilderController constructor comment.
+	 */
 	public ContinueButtonController(Main main) {
 		this.main = main;	}
 
+	/**
+	 * Coordinate reaction to clicking the LetterCraze splash screen Continue button, creating and entering the menu screen.
+	 * <p>
+	 * @param e controllers.ContinueButtonController.actionPerformed(ActionEvent)
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		ProgressIO.saveUnlockedNum(1);
--- src/controllers/DeleteLevelButtonController.java
+++ src/controllers/DeleteLevelButtonController.java
@@ -16,12 +16,26 @@ import builderBoundary.SaveSelectionWindow;
 import entities.Model;
 import entities.Word;
 
+/**
+ *  Controls all actions related to the Builder Delete Menu Button, 
+ *  specifically pressing the Delete button on the Delete Level window 
+ *  after selecting a level from the drop-down menu to remove the level from disk.
+ * <p>
+ * Creation date: (12/15/16 6:25:12 PM)
+ * @author Ian Vossoughi
+ */
 public class DeleteLevelButtonController implements ActionListener {
 
+	/** The highest level entity model.*/
 	private Model m;
+	
+	/** The builder boundary that we are manipulating and using to begin the action. */
 	private BuildField b;
 	//private SaveSelectionWindow saveSelectionWindow;
 	
+	/**
+	 * DeleteLevelButtonController constructor comment.
+	 */
 	public DeleteLevelButtonController(Model m, BuildField b){
 		this.m = m;
 		this.b = b;
@@ -29,12 +43,21 @@ public class DeleteLevelButtonController implements ActionListener {
 	}
 
 	// NOTE: Don't test this...
+	/**
+	 * Coordinate reaction to clicking the Delete level window's Delete button, removing the respective level from disk.
+	 * <p>
+	 * @param ae controllers.DeleteLevelButtonController.actionPerformed(ActionEvent)
+	 */
 	@Override
 	public void actionPerformed(ActionEvent ae) {
 		int levelNum = m.getLevelSaverInt();
 		this.deleteLevel("levels/" + levelNum);
 	}
 
+	/**
+	 * Find the level matching the passed filename and remove it from memory
+	 * @param filename The name of the level ("levels/" followed by a single number) to be deleted
+	 */
 	public void deleteLevel(String filename){
 		System.out.println("Deleting file: " + filename);
 		File file = new File(filename);
